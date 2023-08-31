ab65ef6c6ceff066f776681bcd1f9e70d568123d
Juan Pablo Castiglione <juanpablocastiglione01@gmail.com>
2023-08-30 22:28:59 -0300

    Falta formulario

--- js/custom.js
+++ js/custom.js
@@ -53,7 +53,7 @@ $('.owl-filter-bar').on('click', '.item', function (e) {
 /** google_map js **/
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
+        center: new google.maps.LatLng(-34.5729119, -58.5482433),
         zoom: 18,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
